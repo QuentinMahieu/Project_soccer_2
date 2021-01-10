f4cac6397158ee61bd5ed18bea7b9e5bca6eec93
Louis Razuki <louis@razuki.comm>
2021-01-10 18:39:24 +1100

    getting ready for a merge

--- static/js/louis.js
+++ static/js/louis.js
@@ -1,23 +1,25 @@
 function init(country, year, measure) {
   buildHero(country);
   buildTable(country, year, measure);
+  buildChart(country, year, measure);
 }
 
 function buildHero(country) {
   // Load in the data
-  d3.csv(`Test_transfer.csv`).then((data, error) => {
+  d3.json("/financials/hero/data").then((data, error) => {
     if (error) throw error;
     // Log the country selected in the dropdown
     var country = d3.select("#league").node().value;
     // Use to filter the dataset
+
     data = data.filter((d) => d.League == country);
     //Format the data and prepare for it to be plotted
     transfer = [];
     year = [];
     data.forEach((d) => {
-      d.Transfer_spend = +d.Transfer_spend;
-      year.push(d.Year);
-      transfer.push(d.Transfer_spend);
+      d.Transfer_spend = +d.Transfer_Spend;
+      year.push(d.year);
+      transfer.push(d.Transfer_Spend);
     });
     // Set up parameters for Plotly
     var trace = {
@@ -43,9 +45,67 @@ function buildHero(country) {
   });
 }
 
+function buildChart(country, year, measure) {
+  var country = d3.select("#league").node().value;
+  var year = d3.select("#year").node().value;
+  var measure = d3.select("#measure").node().value;
+
+  d3.json(`/financials/comparison/data`).then((data2, error) => {
+    data2 = data2.filter((d) => d.League == country);
+    data2 = data2.filter((d) => d.Year == year);
+
+    var league_average = 0;
+    var top_4 = 0;
+    var bottom_4 = 0;
+
+    function graphData(group) {
+      data = data2.filter((d) => d.Group == group);
+      var league_transfer_spend = 0;
+      var league_wages = 0;
+      var league_player_value = 0;
+      data.forEach((d) => {
+        league_transfer_spend = d.Transfers;
+        league_wages = d.Wages;
+        league_player_value = d.Value;
+      });
+
+      var value = 0;
+      if (measure == "Wages") {
+        value = league_wages;
+      } else if (measure == "Transfers") {
+        value = league_transfer_spend;
+      } else if (measure == "Value") {
+        value = league_player_value;
+      }
+      return value;
+    }
+    var top4 = graphData("Top 4");
+    var bottom4 = graphData("Bottom 4");
+    var league = graphData("League");
+
+    values = [top4, league, bottom4];
+    labels = ["Top 4", "League Average", "Bottom 4"];
+    console.log(values);
+    var trace = {
+      x: labels,
+      y: values,
+      type: "bar",
+    };
+    var data = [trace];
+    var layout = {
+      title: "Comparison",
+    };
+    d3.select("#comparison").append("div").attr("id", "comparison_chart");
+
+    // Build plot for Hero graph
+    Plotly.newPlot("comparison_chart", data, layout);
+  });
+}
+
 function buildTable(country, year, measure) {
-  d3.csv(`Test_table.csv`).then((data, error) => {
+  d3.json(`/financials/top4/data`).then((data, error) => {
     if (error) throw error;
+
     // Log the country selected in the dropdown
     var country = d3.select("#league").node().value;
     var year = d3.select("#year").node().value;
@@ -54,34 +114,52 @@ function buildTable(country, year, measure) {
     data = data.filter((d) => d.League == country);
     data = data.filter((d) => d.Year == year);
     //Format the data and prepare for it to be plotted
+
     teams = [];
     position = [];
     transfer_spend = [];
     wages = [];
+    player_value = [];
     data.forEach((d) => {
       teams.push(d.Team);
-      position.push(d.Position);
+      position.push(d.Ranking);
       transfer_spend.push(d.Transfer_spend);
       wages.push(d.Player_Wages);
+      player_value.push(d.Average_Player_Value);
     });
+
     if (measure == "Wages") {
       financials = wages;
-      title = "Avg. Player Wages (Euros)";
+      title = "Avg. Player Wages";
     } else if (measure == "Transfers") {
       financials = transfer_spend;
-      title = "3 Yr MA Transfer Spend (Euros)";
+      title = "Average Transfer Spend";
+    } else if (measure == "Value") {
+      financials = player_value;
+      title = "Average Player Value";
     }
 
-    d3.csv(`Test_graph.csv`).then((data2, error) => {
+    d3.json(`/financials/comparison/data`).then((data2, error) => {
       data2 = data2.filter((d) => d.League == country);
       data2 = data2.filter((d) => d.Year == year);
-      data2 = data2.filter((d) => d.Measure == measure);
+      data2 = data2.filter((d) => d.Group == "Top 4");
 
-      averaged = 0;
+      league_transfer_spend = [];
+      league_wages = [];
+      league_player_value = [];
       data2.forEach((d) => {
-        averaged = d.League_Average;
+        league_transfer_spend.push(d.Transfers);
+        league_wages.push(d.Wages);
+        league_player_value.push(d.Value);
       });
-      console.log(averaged);
+
+      if (measure == "Wages") {
+        averaged = league_wages;
+      } else if (measure == "Transfers") {
+        averaged = league_transfer_spend;
+      } else if (measure == "Value") {
+        averaged = league_player_value;
+      }
       d3.select("#average")
         .append("p")
         .attr("id", "average_copy")
@@ -102,9 +180,14 @@ function buildTable(country, year, measure) {
           subtract = +subtract;
           difference.push(subtract);
         });
+      } else if (measure == "Value") {
+        player_value.forEach((d) => {
+          var subtract = d - averaged;
+          subtract = +subtract;
+          difference.push(subtract);
+        });
       }
 
-      console.log(difference);
       var values = [position, teams, financials, difference];
 
       var data = [
@@ -137,24 +220,6 @@ function buildTable(country, year, measure) {
   });
 }
 
-function getColor(d) {
-  return d > 100000
-    ? "#800026"
-    : d > 5
-    ? "#BD0026"
-    : d > 4
-    ? "#E31A1C"
-    : d > 3
-    ? "#FC4E2A"
-    : d > 2
-    ? "#FD8D3C"
-    : d < 0
-    ? "#FEB24C"
-    : d < -10000
-    ? "#FED976"
-    : "#FFEDA0";
-}
-
 var selection = d3.select("#league");
 selection.on("change", updateData);
 var selected = d3.select("#measure");
@@ -166,6 +231,7 @@ function updateData() {
   d3.select("plot").remove();
   d3.select("#transfer_copy").remove();
   d3.select("#average_copy").remove();
+  d3.select("#comparison_chart").remove();
   var value = d3.select("#league").node().value;
   if (value === "Spain") {
     country = value;
@@ -179,4 +245,4 @@ function updateData() {
   init(country, year, measure);
 }
 
-init("England", "2019", "Wages");
+init("England", "2016", "Wages");
